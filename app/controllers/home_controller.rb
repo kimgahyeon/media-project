a54a36ca99ea04aba178e1ce433bf78c8f2a216c
김가현 <Kimgahyeon@gimgahyeon-ui-MacBookPro.local>
2019-04-05 17:16:23 +0900

    이더리움 연결 전 커밋

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   # before_action :authenticate_user!
   def index
+    @projects = Project.all
   end
 
   def mypage
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  before_action :load_object, only: [:show, :edit, :update, :destroy]
+
   def index
     @projects = Project.all
   end
@@ -15,17 +17,30 @@ class ProjectsController < ApplicationController
   end
 
   def update
+    if @project.update(project_params)
+        redirect_to @project
+      else
+        render :edit
+    end
+  end
+
+  def edit
   end
 
   def destroy
+    @project.destroy
+    redirect_to projects_path
   end
 
   def show
-    @project = Project.find(params[:id])
   end
 
   private
   def project_params
     params.require(:project).permit(:title, :user_id, :detail, :condition, :location, :time, :number, :desc, :contact)
+  end
+
+  def load_object
+    @project = Project.find(params[:id])
   end
 end
